b69051e28f3890b1caa987338c0f05797abbf330
Nurik <nurdosjurabekov14@gmail.com>
2024-04-26 02:09:42 +0500

    End taking data form html code

---
+++ src/parser/get_data_from_urls.py
@@ -0,0 +1,41 @@
+import asyncio
+import time
+import datetime
+from concurrency_get_urls import main
+from bs4 import BeautifulSoup
+from aiohttp import ClientSession
+
+
+
+URLS = main()
+
+async def get_page(session: ClientSession, url: str) -> BeautifulSoup:
+        async with session.get(url) as response:
+            return await response.text()
+
+async def main()-> list:
+    async with ClientSession() as session:
+        tasks = [get_page(session, url) for url in URLS]
+        res = await asyncio.gather(*tasks)
+
+        return [BeautifulSoup(response, 'html.parser') for response in res]
+
+
+loop = asyncio.get_event_loop()
+soups = loop.run_until_complete(main())
+questions = list()
+answers = list()
+
+for soup in soups:
+    questions.append(soup.h1.text)
+    if len(soup.find('div', class_='card-body py-3').find_all('p')) >=  2:
+        answers.append(soup.find('div', class_='card-body py-3').find_all('p')[1].getText(strip=True))
+    else: answers.append(soup.find('div', class_='card-body py-3').find_all('p')[0].getText(strip=True))
+
+
+
+
+
+
+
+
